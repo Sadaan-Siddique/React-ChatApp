9a2c95a202a6efbca8c9dd48ff7187096a9a5e19
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-03-07 23:26:44 +0500

    CHatAPp

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import {auth} from'./firebase';
-import {useAuthState} from'react-firebase-hooks/auth';
-import Chat from "./components/Chat";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Chat from './components/Chat';
+
+import { auth } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
 
 function App() {
-  // JS
-  const [user] = useAuthState(auth)
-  // console.log(user);
-
-  const style = {
-    appContainer: `max-w-[728px] mx-auto text-center`,
-    sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
-  };
-  // HTML
+  const [user] = useAuthState(auth);
+  //  console.log(user)
   return (
     <div className={style.appContainer}>
-      <section className={style.sectionContainer}>
+      <section className='{style.sectionContainer}'>
         {/* Navbar */}
         <Navbar />
-        {/* {user ? <Chat /> : null} */}
-        <Chat/>
+        {user ? <Chat /> : null}
       </section>
     </div>
   );
